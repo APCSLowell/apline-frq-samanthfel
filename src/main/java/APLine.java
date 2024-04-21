3f7ac67c1926b6082ff47aa2e18955734d00e25e
samanthfel <143027804+samanthfel@users.noreply.github.com>
2024-04-21 16:44:01 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,15 @@
 public class APLine
 {
-  /* your code here */
-  
+  private in a, b, c;
+  public APLine(int a1, int b1, int c1){
+    a = a1;
+    b = b1;
+    c = c1;
+  }
+  public double getSlope(){
+    return (-(double)a/b);
+  }
+  public boolean isOnline(int x, int y){
+    return a*x + b*x + c ==0;
+  }
 }
